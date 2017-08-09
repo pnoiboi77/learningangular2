03eeb0a8d0a5fc310a15160315acdd1634674916
Bernard Espanola <espanola.bernard@gmail.com>
2017-08-09 00:14:37 -0700

    configured routing. configured routes, implemented routerLink in action and provided a router-outlet tag.

--- app/app.component.js
+++ app/app.component.js
@@ -17,7 +17,7 @@ var AppComponent = (function () {
 AppComponent = __decorate([
     core_1.Component({
         selector: 'pm-app',
-        template: '<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>',
+        template: "\n    <nav class=\"navbar navbar-default navbar-static-top\">\n        <div class=\"container-fluid\">\n            <div class=\"navbar-header\">\n                <button type=\"button\" \n                class=\"navbar-toggle collapsed\" data-toggle=\"collapse\"\n                 data-target=\"#navbar\" aria-expanded=\"false\" aria-controls=\"navbar\">\n                    <span class=\"sr-only\">Toggle navigation</span>\n                    <span class=\"icon-bar\"></span>\n                    <span class=\"icon-bar\"></span>\n                    <span class=\"icon-bar\"></span>\n                </button>\n                <a class=\"navbar-brand\" href=\"#\">Acme</a>\n            </div>\n            <div id=\"navbar\" class=\"navbar-collapse collapse\">\n                <ul class=\"nav navbar-nav\">\n                    <li><a [routerLink]=\"['/welcome']\">Home</a></li>\n                    <li><a [routerLink]=\"['/products']\">Products</a></li>\n                </ul>          \n            </div><!--/.nav-collapse -->\n        </div>\n    </nav>\n    <div class=\"container-fluid\">\n        <div class=\"row\">\n            <div class=\"col-md-10 col-md-offset-1\">\n                <router-outlet></router-outlet>\n            </div>\n        </div>\n    </div>\n    ",
         providers: [product_service_1.ProductService]
     })
 ], AppComponent);
